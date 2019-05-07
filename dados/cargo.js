17733310b15a9b3f898a28a43b9fbcfb830a6807
Pablo Fernandes <pablofern87@gmail.com>
2019-05-07 11:53:17 -0300

    adicioando novo cargo de programadorr inicial

--- dados/cargo.js
+++ dados/cargo.js
@@ -5,6 +5,18 @@ const cargo = {
     desenvolvedor: {
         cargaHorariaSemanal: _padrao.cargaHorariaSemanal,
         fatorEsperadoCargaHorariaSemanal: _padrao.fatorEsperadoCargaHorariaSemanal
+    },
+    desenvolvedorInicial: {
+        cargaHorariaSemanal: _padrao.cargaHorariaSemanal,
+        fatorEsperadoCargaHorariaSemanal: {
+            '1': 0,
+            '2': 0.5,
+            '3': 0.5,
+            '4': 0.5,
+            '5': 0.5,
+            '6': 0.5,
+            '7': 0.25,
+        }
     }
 }
 
