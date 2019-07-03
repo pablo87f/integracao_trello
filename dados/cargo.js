d618b947c7d19fe2228136178f47bae15e97151e
Pablo Fernandes <pablofern87@gmail.com>
2019-07-03 16:48:37 -0300

    criando novo cargo com sabado para estudos

--- dados/cargo.js
+++ dados/cargo.js
@@ -17,7 +17,26 @@ const cargo = {
             '6': 0.5,
             '7': 0.25,
         }
-    }
+    },
+    desenvolvedorSabadoEstudos: {
+        cargaHorariaSemanal: _padrao.cargaHorariaSemanal,
+        fatorEsperadoCargaHorariaSemanal: {
+            ..._padrao.fatorEsperadoCargaHorariaSemanal,
+            '7': 0,
+        }
+    },
+    desenvolvedorInicialSabadoEstudos: {
+        cargaHorariaSemanal: _padrao.cargaHorariaSemanal,
+        fatorEsperadoCargaHorariaSemanal: {
+            '1': 0,
+            '2': 0.5,
+            '3': 0.5,
+            '4': 0.5,
+            '5': 0.5,
+            '6': 0.5,
+            '7': 0,
+        }
+    },
 }
 
 module.exports = cargo
