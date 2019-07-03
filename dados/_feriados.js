7cc1270b63f445907417dbb4594e914fdf5b876e
Pablo Fernandes <pablofern87@gmail.com>
2019-07-03 17:14:50 -0300

    adicionando os feriádos do ano de 2019

--- dados/_feriados.js
+++ dados/_feriados.js
@@ -3,6 +3,12 @@
 
 const _feriados = {
     '01/05/2019': 'Dia do trabalho',
+    '29/06/2019': 'Dia de São Jose',
+    '07/09/2019': 'Dia da independência do Brasil',
+    '12/10/2019': 'Dia de Nossa Senhora Aparecida',
+    '02/11/2019': 'Dia de Finados',
+    '15/11/2019': 'Dia da Proclamação da República',
+    '25/12/2019': 'Natal',
 }
 
 module.exports = _feriados
