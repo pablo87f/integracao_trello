9f9620756b0d908be2f51871c33f5fe405ec6884
Pablo <Fernandes>
2020-03-05 07:30:12 -0300

    feat(cargo): - Atualizando a estimativa esperada do cargo - Desenvolvedo sabado estudos

--- src/dados/cargo.js
+++ src/dados/cargo.js
@@ -33,15 +33,15 @@ const cargo = {
     },
     desenvolvedorInicialSabadoEstudos: {
         cargaHorariaDia: 8,
-        maxFatorEsperadoDia: 0.5,
+        maxFatorEsperadoDia: 0.4,
         cargaHorariaSemanal: _padrao.cargaHorariaSemanal,
         fatorEsperadoCargaHorariaSemanal: {
             '1': 0,
-            '2': 0.5,
-            '3': 0.5,
-            '4': 0.5,
-            '5': 0.5,
-            '6': 0.5,
+            '2': 0.4,
+            '3': 0.4,
+            '4': 0.4,
+            '5': 0.4,
+            '6': 0.4,
             '7': 0,
         }
     },
