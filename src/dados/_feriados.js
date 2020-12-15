58d3ae2f94a4e5a5509605dfe9d9c34bc9721a9d
Pablo Fernandes <pablofern87@gmail.com>
2020-12-15 17:02:12 -0300

    feat(feriados): add feriados de fim de ano

--- src/dados/_feriados.js
+++ src/dados/_feriados.js
@@ -9,6 +9,10 @@ const _feriados = {
     '02/11/2019': 'Dia de Finados',
     '15/11/2019': 'Dia da Proclamação da República',
     '25/12/2019': 'Natal',
+    '24/12/2020': 'Véspera Natal',
+    '25/12/2020': 'Natal',
+    '31/12/2020': 'Véspera ano novo',
+    '01/12/2021': 'Dia internacional da paz',
 }
 
 module.exports = _feriados
